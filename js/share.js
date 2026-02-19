9cee37c821c801435c5fdc3dfa626e699482e76f
Biswas542 <biswasparajuli224@gmail.com>
2026-02-19 15:09:16 +0545

    new update

--- js/share.js
+++ js/share.js
@@ -6,3 +6,20 @@ fetch("header.html")
 fetch("footer.html")
   .then(res => res.text())
   .then(data => document.getElementById("footer").innerHTML = data);
+
+
+
+
+// nav button js 
+   function toggleMenu() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function (event) {
+      if (!event.target.closest('.menu-btn')) {
+        var dropdown = document.getElementById("myDropdown");
+        if (dropdown.classList.contains('show')) {
+          dropdown.classList.remove('show');
+        }
+      }
+    }
